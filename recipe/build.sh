17b861c7e338bcc940668fd1105663c922681d2f
Chris Burr <christopher.burr@cern.ch>
2025-02-09 23:21:05 +0100

    Add more patches

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,9 @@
 
 # See: https://github.com/prefix-dev/rattler-build/issues/1418
 tar xvf YODA-*.tar.gz --strip-components=1
-patch --batch -p1 < "$RECIPE_DIR/0001-Fix-setting-rpath-when-installing-on-macOS.patch"
+for patch_name in 0001-Fix-setting-rpath-when-installing-on-macOS.patch 0002-Support-overriding-install_name_tool.patch 0003-Do-not-link-against-libpython.patch; do
+    patch --batch -p1 < "$RECIPE_DIR/${patch_name}"
+done
 
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
